a09fa33179648288410227cc6d066195882f82cf
Gabigabe <mhammed.gabigabe@gmail.com>
2026-02-03 15:09:00 +0100

    chore(migrations) : ajout des migrations initiales

---
+++ database/migrations/2026_02_03_111341_create_jours_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jours', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->time('heure_debut');
+            $table->time('heure_fin');
+            $table->boolean('est_ouvert')->default(true);
+            $table->foreignId('horaire_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jours');
+    }
+};
