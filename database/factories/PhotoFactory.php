40e3d7ef39c9c95277c2ed19f86475ea11a051c6
Gabigabe <mhammed.gabigabe@gmail.com>
2026-02-03 23:33:12 +0100

    Ajout de factorie PhotoFactory

---
+++ database/factories/PhotoFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Restaurant;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Photo>
+ */
+class PhotoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'url' => 'restaurants/default.jpg',
+            'restaurant_id' => Restaurant::factory(),
+        ];
+    }
+}
