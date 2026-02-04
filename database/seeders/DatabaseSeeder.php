49cacff5790efc0c08a2350bcfaafb7fb8075aa7
Gabigabe <mhammed.gabigabe@gmail.com>
2026-02-04 10:14:21 +0100

    DatabaseSeeder: seeders propres et fonctionnels avec test et validation via Tinker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\Horaire;
+use App\Models\Menu;
+use App\Models\Restaurant;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\TypeCuisine;
+use App\Models\Jour;
+use App\Models\Plat;
+use App\Models\Photo;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +22,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $types = ['Marocaine', 'Italienne', 'Chinoise', 'Française', 'Mexicaine'];
+        foreach ($types as $typeNom) {
+            TypeCuisine::create(['nom' => $typeNom]);
+        }        
+
+        $users->each(function($user){
+            $restaurants = Restaurant::factory(2)->for($user)->create();
+
+            $restaurants->each(function($restaurant){
+                $horaire = Horaire::factory()->for($restaurant)->create();
+
+                $jours = ['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche'];
+                foreach ($jours as $jourNom) {
+                    Jour::create([
+                        'nom' => $jourNom,
+                        'heure_debut' => '09:00',
+                        'heure_fin' => '23:00',
+                        'est_ouvert' => true,
+                        'horaire_id' => $horaire->id,
+                    ]);
+                }
+                $menus = Menu::factory(2)->for($restaurant)->create();
+                $menus->each(function($menu) {
+                    Plat::factory(5)->for($menu)->create();
+                });
+
+                Photo::factory(3)->for($restaurant)->create();
+            });
+        });
+        
+        $restaurants = Restaurant::all();
+        $typesCuisine = TypeCuisine::all();
+        foreach ($restaurants as $restaurant) {
+            $restaurant->typesCuisines()->attach($typesCuisine->random(rand(1,3))->pluck('id')->toArray());
+        }
     }
 }
