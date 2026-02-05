50459260b957c790011834bb72628864d66959a8
Gabigabe <mhammed.gabigabe@gmail.com>
2026-02-05 16:22:54 +0100

    feat: assigner le role a l'utilisateur lors de l'inscription et ajouter deux autres champs

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Str;
 
 class RegisteredUserController extends Controller
 {
@@ -39,7 +41,10 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
         ]);
+        $user->assignRole($request->role);
 
         event(new Registered($user));
 
